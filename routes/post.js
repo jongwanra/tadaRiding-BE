b6aa21d86fa1044c9d0fd987b406d2697742252c
changwoo Jeong <jcw9828@gmail.com>
2021-10-13 13:55:00 +0900

    게시글 수정,삭제

--- routes/post.js
+++ routes/post.js
@@ -55,6 +55,47 @@ router.post('/', auth.isAuth, async (req, res) => {
 });
 
 // 게시글 수정 API
+router.put('/:postUid', async (req, res) => {
+  const { postUid } = req.params
+  const {
+    postTitle,
+    postDesc,
+    limitedUserNum,
+    origin,
+    destination,
+    postImage,
+    startTime,
+  } = req.body;
+  const post = await Post.findOne({ postUid });
+
+  if (postUid === post.postUid) {
+    await Post.updateOne({ postUid: postUid }, { $set: { 
+      postTitle,
+      postDesc,
+      limitedUserNum,
+      origin,
+      destination,
+      postImage,
+      startTime,
+     } });
+     res.status(201).send({ success: true, msg: '성공적으로 게시글이 수정되었습니다.' })
+  } else {
+    res.status(500).send({ success: false, msg: '게시글 수정 중 에러가 발생했습니다.' })
+  }
+})
+
 // 게시글 삭제 API
+router.delete('/:postUid', async (req, res) => {
+  const { postUid } = req.params;
+  const post = await Post.findOne({ postUid })
+
+  if (postUid === post.postUid) {
+    await Post.deleteOne({ postUid })
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
 
 module.exports = router;
